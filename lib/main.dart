05f0d901a6267f06d41baad1864ae94f80f09444
Srana29 <sakshamrana12345678@gmail.com>
2021-11-23 19:38:18 +0530

    flutter_sqflite

---
+++ lib/main.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+import 'sql_helper.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Remove the debug banner
+        debugShowCheckedModeBanner: false,
+        title: 'myshapeshub.com',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // All journals
+  List<Map<String, dynamic>> _journals = [];
+
+  bool _isLoading = true;
+  // This function is used to fetch all data from the database
+  void _refreshJournals() async {
+    final data = await SQLHelper.getItems();
+    setState(() {
+      _journals = data;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshJournals(); // Loading the diary when the app starts
+  }
+
+  TextEditingController _titleController = new TextEditingController();
+  TextEditingController _descriptionController = new TextEditingController();
+
+  // This function will be triggered when the floating button is pressed
+  // It will also be triggered when you want to update an item
+  void _showForm(int? id) async {
+    if (id != null) {
+      // id == null -> create new item
+      // id != null -> update an existing item
+      final existingJournal =
+      _journals.firstWhere((element) => element['id'] == id);
+      _titleController.text = existingJournal['title'];
+      _descriptionController.text = existingJournal['description'];
+    }
+
+    showModalBottomSheet(
+        context: context,
+        elevation: 5,
+        builder: (_) => Container(
+          padding: EdgeInsets.all(15),
+          width: double.infinity,
+          height: 300,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: InputDecoration(hintText: 'Title'),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: _descriptionController,
+                decoration: InputDecoration(hintText: 'Description'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  // Save new journal
+                  if (id == null) {
+                    await _addItem();
+                  }
+
+                  if (id != null) {
+                    await _updateItem(id);
+                  }
+
+                  // Clear the text fields
+                  _titleController.text = '';
+                  _descriptionController.text = '';
+
+                  // Close the bottom sheet
+                  Navigator.of(context).pop();
+                },
+                child: Text(id == null ? 'Create New' : 'Update'),
+              )
+            ],
+          ),
+        ));
+  }
+
+// Insert a new journal to the database
+  Future<void> _addItem() async {
+    await SQLHelper.createItem(
+        _titleController.text, _descriptionController.text);
+    _refreshJournals();
+  }
+
+  // Update an existing journal
+  Future<void> _updateItem(int id) async {
+    await SQLHelper.updateItem(
+        id, _titleController.text, _descriptionController.text);
+    _refreshJournals();
+  }
+
+  // Delete an item
+  void _deleteItem(int id) async {
+    await SQLHelper.deleteItem(id);
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text('Successfully deleted a journal!'),
+    ));
+    _refreshJournals();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('myshapeshub.com'),
+      ),
+      body: _isLoading
+          ? Center(
+        child: CircularProgressIndicator(),
+      )
+          : ListView.builder(
+        itemCount: _journals.length,
+        itemBuilder: (context, index) => Card(
+          color: Colors.orange[200],
+          margin: EdgeInsets.all(15),
+          child: ListTile(
+              title: Text(_journals[index]['title']),
+              subtitle: Text(_journals[index]['description']),
+              trailing: SizedBox(
+                width: 100,
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () => _showForm(_journals[index]['id']),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () =>
+                          _deleteItem(_journals[index]['id']),
+                    ),
+                  ],
+                ),
+              )),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _showForm(null),
+      ),
+    );
+  }
+}
